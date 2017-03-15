f90a09417aaebc0f48bff48e29b5e1bbfc811daf
Beata-MacBook <Beata-MacBook@JAKUB77D0>
2017-03-15 09:34:50 +0100

    calculating inverse affine transform

--- gdal_test/gdal_test.cpp
+++ gdal_test/gdal_test.cpp
@@ -12,9 +12,10 @@ int main()
 	GDALDataset *poDataset;
 	GDALAllRegister();
 	double adfGeoTransform[6];
-	double Xp;
-	double Yp;
-	poDataset = (GDALDataset *)GDALOpen("C:\\Users\\Beata-MacBook\\Desktop\\TMC\\gdal_test\\test.tiff", GA_ReadOnly);
+	double Xw, Yw, Xp, Yp, Wx, Wy, W;
+	
+
+	poDataset = (GDALDataset *)GDALOpen("C:\\Users\\Beata-MacBook\\Desktop\\TMC\\gdal_test\\landsat.tif", GA_ReadOnly);
 
 	if (poDataset == NULL) {
 
@@ -43,10 +44,22 @@ int main()
 				adfGeoTransform[1], adfGeoTransform[5]);
 		}
 
-		std::cout << "Podaj wspolrzedna x: ";
-		std::cin >> Xp;
-		std::cout << "Podaj wspolrzedna y: ";
-		std::cin >> Yp;
+		//std::cout << "Podaj wspolrzedna P: ";
+		//std::cin >> Xw;
+		//std::cout << "Podaj wspolrzedna L: ";
+		//std::cin >> Yw;
+
+		Xw = 13.0;
+		Yw = 14.0;
+		W = adfGeoTransform[1] * adfGeoTransform[5] - adfGeoTransform[4] * adfGeoTransform[2];
+		Wx = adfGeoTransform[5] * (Xw - adfGeoTransform[0]) - adfGeoTransform[2] * (Yw - adfGeoTransform[3]);
+		Wy = adfGeoTransform[1] * (Yw - adfGeoTransform[3]) - adfGeoTransform[4] * (Xw - adfGeoTransform[0]);
+
+		Xp = Wx / W;
+		Yp = Wy / W;
+		//printf("Xp: %d\nYp: %d ", Xp, Yp);
+
+		
 
 	}
 
