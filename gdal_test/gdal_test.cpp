b1441756600663da8bdf9e9cafd698eb6bd174a0
Beata-MacBook <Beata-MacBook@JAKUB77D0>
2017-03-15 08:57:06 +0100

    initial commit

---
+++ gdal_test/gdal_test.cpp
@@ -0,0 +1,54 @@
+﻿// gdal_test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "gdal_priv.h"
+#include "cpl_conv.h"
+#include <iostream>
+
+
+int main()
+{	
+	GDALDataset *poDataset;
+	GDALAllRegister();
+	double adfGeoTransform[6];
+	double Xp;
+	double Yp;
+	poDataset = (GDALDataset *)GDALOpen("C:\\Users\\Beata-MacBook\\Desktop\\TMC\\gdal_test\\test.tiff", GA_ReadOnly);
+
+	if (poDataset == NULL) {
+
+		printf("Dataset is null");
+	}
+	else {
+		
+		//adfGeoTransform[0] /* top left x */
+		//adfGeoTransform[1] /* w-e pixel resolution */
+		//adfGeoTransform[2] /* 0 */
+		//adfGeoTransform[3] /* top left y */
+		//adfGeoTransform[4] /* 0 */
+		//adfGeoTransform[5] /* n-s pixel resolution (negative value) */
+	
+		printf("Nazwa sterownika: %s%s\n", poDataset->GetDriver()->GetDescription(), poDataset->GetDriver()->GetMetadataItem(GDAL_DMD_LONGNAME));
+		printf("Rozmiar: %dx%d Liczba kanalow: %d\n",
+			poDataset->GetRasterXSize(), poDataset->GetRasterYSize(),
+			poDataset->GetRasterCount());
+		if (poDataset->GetProjectionRef() != NULL)
+			printf("Projection is `%s'\n", poDataset->GetProjectionRef());
+		if (poDataset->GetGeoTransform(adfGeoTransform) == CE_None)
+		{
+			printf("Origin = (%.6f,%.6f)\n",
+				adfGeoTransform[0], adfGeoTransform[3]);
+			printf("Pixel Size = (%.6f,%.6f)\n",
+				adfGeoTransform[1], adfGeoTransform[5]);
+		}
+
+		std::cout << "Podaj wspolrzedna x: ";
+		std::cin >> Xp;
+		std::cout << "Podaj wspolrzedna y: ";
+		std::cin >> Yp;
+
+	}
+
+}
+
